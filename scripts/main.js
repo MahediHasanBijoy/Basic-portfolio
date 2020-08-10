2a1e17a4b8da50a22323aafbd8a352c437eb85ce
MahediHasanBijoy <bijoycse2014@gmail.com>
2020-08-11 01:30:27 +0600

    First Commit!

---
+++ scripts/main.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+
+	var $header = $('header');
+	var $sticky = $header.before($header.clone().addClass("sticky"));
+
+	$(window).on("scroll",function(){
+		var scrollFromTop = $(window).scrollTop();
+		$('body').toggleClass("scroll",(scrollFromTop > 150));
+	});
+
+	//SMOOTH SCROLL FOR SECTION POSITION IN WINDOW
+	$('.menu li a[href^="#"]').on('click',function(e){
+		e.preventDefault();
+
+		var target = $(this.hash);
+
+		if(target.length){
+			$('html, body').stop().animate({
+				scrollTop: target.offset().top -57
+			}, 1000 );
+		}
+	});
+
+
+	//MASONRY
+	$('.grid').masonry({
+		//Options
+		itemSelector: '.grid-item',
+		columnWidth: 120,
+		fitWidth: true,
+		gutter: 0 
+	});
+
+	//SLICK SLIDER
+	$('.slider').slick({
+		autoplay: true,
+		autoplaySpeed: 1500,
+		centerMode: true,
+		slidesToShow: 3,
+		arrows: true,
+		prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+		nextArrow: '<button type="button" class="slick-next">Next</button>',
+		responsive: [
+			{
+				breakpoint: 900,
+				settings:{
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 668,
+				settings:{
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+
+	//RESPONSIVE MENU START 
+	var body = $('body');
+	var menuTrigger = $('.js-menu-trigger');
+	var mainOverlay = $('.js-main-overlay');
+
+	mainOverlay.on('click',function(){
+		body.removeClass('menu-is-active');
+	});
+
+	menuTrigger.on('click',function(){
+		body.addClass('menu-is-active');
+	});
+
+	$('.menu li a').on('click', function(){
+		$('body').removeClass('menu-is-active');
+	});
+
+
+
+
+
+});
